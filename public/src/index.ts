c6ca86859814df47f0a8453042d06e20e61bb7f4
Faka2005 <115738354+Faka2005@users.noreply.github.com>
2024-12-04 21:00:36 +0100

    rgfhrsfbxcn v

--- public/src/index.ts
+++ public/src/index.ts
@@ -15,7 +15,7 @@ app.use(cors({
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html')); // Utilisation correcte de __dirname
+  res.send('API is running');//Je veux qu'il me renvoie ./inddex.html
 });
 app.get('/courses', (req, res) => {
   const courses = [
